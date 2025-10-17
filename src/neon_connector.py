1712dfc575b68c684afbc0921280036612f575bd
Jussi Virtanen <jussi.virtanen2@protonmail.com>
2025-10-17 15:28:07 +0300

    add working test file to connect to Neon

---
+++ src/neon_connector.py
@@ -0,0 +1,25 @@
+import os
+
+import psycopg
+from dotenv import load_dotenv
+import polars as pl
+
+load_dotenv()
+
+conn_string = os.getenv("DATABASE_URL")
+
+try:
+    with psycopg.connect(conn_string) as conn:
+        print("Connection established")
+        with conn.cursor() as cur:
+            # Fetch all rows from the books table
+            cur.execute("SELECT * FROM asset_management_test.asset_transactions;")
+            data = cur.fetchall()
+            columns = [desc[0] for desc in cur.description]
+            df = pl.DataFrame(data, schema=columns, orient="row")
+            with pl.Config(tbl_cols=-1):
+                print(df)
+
+except Exception as e:
+    print("Connection failed.")
+    print(e)
