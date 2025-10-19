7350159c3150a80a345b7e5a76f87260bb832d33
Jussi Virtanen <jussi.virtanen2@protonmail.com>
2025-10-19 19:35:05 +0300

    test postgresconnector with new database

--- src/neon_connector.py
+++ src/neon_connector.py
@@ -1,27 +1,38 @@
 import os
-
 import psycopg
 from dotenv import load_dotenv
 import polars as pl
+from db_connector import PostgresConnector
 
 # Load environment variables from .env file
 load_dotenv()
 # Get connection string
 conn_string = os.getenv("DATABASE_URL")
-
+owner_id = 10
 
 try:
-    with psycopg.connect(conn_string) as conn:
-        print("Connection established")
-        with conn.cursor() as cur:
-            # Fetch all rows from the books table
-            cur.execute("SELECT * FROM asset_management_test.asset_transactions;")
-            data = cur.fetchall()
-            columns = [desc[0] for desc in cur.description]
-            df = pl.DataFrame(data, schema=columns, orient="row")
-            with pl.Config(tbl_cols=-1):
-                print(df)
+    with PostgresConnector() as db:
+        active_assets_df = db.get_active_assets(owner_id)
+        transactions_df = db.get_portfolio_transactions(owner_id)
+        print(active_assets_df)
+        print(transactions_df)
 
 except Exception as e:
     print("Connection failed.")
     print(e)
+
+# try:
+#     with psycopg.connect(conn_string) as conn:
+#         print("Connection established")
+#         with conn.cursor() as cur:
+#             # Fetch all rows from the books table
+#             cur.execute("SELECT * FROM asset_management_test.asset_transactions;")
+#             data = cur.fetchall()
+#             columns = [desc[0] for desc in cur.description]
+#             df = pl.DataFrame(data, schema=columns, orient="row")
+#             with pl.Config(tbl_cols=-1):
+#                 print(df)
+
+# except Exception as e:
+#     print("Connection failed.")
+#     print(e)
